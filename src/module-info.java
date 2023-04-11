740c3d930175fd49fd9794f868e6c9f761ab1216
Al Natour Mazen <115185120+Al-Natour-Mazen@users.noreply.github.com>
2023-04-11 14:59:57 +0200

    Delete src directory

--- src/module-info.java
+++
@@ -1,9 +0,0 @@
-module Project_POO_IHM2 {
-	requires javafx.controls;
-	requires javafx.graphics;
-	
-	opens model to javafx.graphics, javafx.fxml;
-	opens view to javafx.graphics, javafx.fxml;
-	opens main to javafx.graphics, javafx.fxml;
-	//opens controller to javafx.graphics, javafx.fxml;
-}
